a54606009a1bc7878007634e503de991c4c6c506
kenglxn <ken@glxn.net>
2016-06-26 13:17:12 +0200

    add dummy chart.js

--- www/js/app.js
+++ www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
+angular.module('app', ['ionic', 'chart.js', 'app.controllers', 'app.routes', 'app.services', 'app.directives'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,7 +17,7 @@ angular.module('app', ['ionic', 'app.controllers', 'app.routes', 'app.services',
     }
     if (window.StatusBar) {
       // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
+      window.StatusBar.styleDefault();
     }
   });
-})
+});
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,14 @@
 angular.module('app.controllers', [])
-  
+
 .controller('spareKalkulatorCtrl', function($scope) {
-
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+    $scope.series = ['Series A', 'Series B'];
+    $scope.data = [
+        [65, 59, 80, 81, 56, 55, 40],
+        [28, 48, 40, 19, 86, 27, 90]
+    ];
 })
-   
+
 .controller('instillingerCtrl', function($scope) {
-
+  console.log($scope)
 })
-    
